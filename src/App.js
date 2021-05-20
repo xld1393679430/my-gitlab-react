18d5a51f016f7a47848f1b0af9eb57803586e44c
xld1393679430 <x1393679430@outlook.com>
2021-05-20 10:20:19 +0800

    get ip

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
+import IP from './components/IP';
 
 function App() {
   const state = useSelector(state => state)
@@ -15,6 +16,10 @@ function App() {
       {state.count}
       <button onClick={() => handleClick('add')}>+</button>
       <button onClick={() => handleClick('minus')}>-</button>
+
+      <p>
+        <IP />
+      </p>
     </div>
   );
 }
---
+++ src/components/IP/index.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import os from 'os';
+
+
+// todo: 此方式获取IP有问题 有待改进
+const Page = () => {
+    const [ip, setIp] = useState('')
+    useEffect(() => {
+        const ip = os.networkInterfaces().en0[0].address
+        const options = process.argv.slice(2)
+        console.log(os, os.getNetworkInterfaces(), os.platform(), os.networkInterfaces());
+        if (options[0] === '-v') {
+            console.log('v1.0.0')
+        } else {
+            setIp(ip)
+        }
+    }, [])
+
+    return (
+        <span>你的IP: {ip}</span>
+    )
+} 
+
+export default Page
